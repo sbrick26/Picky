990cdbfdf2330b1cff24fe93583888bf60d9629d
sbrick26 <swayambarik@gmail.com>
2016-07-07 13:08:45 -0700

    Initial Commit

---
+++ Picky/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Picky
+//
+//  Created by Swayam Barik on 7/7/16.
+//  Copyright © 2016 Swayam Barik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
